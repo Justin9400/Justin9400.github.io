266c414871fde586bfcfd06d653d20784c6f7377
Justin Kachornvanich <justkachorn@gmail.com>
2025-02-24 20:08:33 -0500

    updated about me section

--- src/App.tsx
+++ src/App.tsx
@@ -132,15 +132,31 @@ function App() {
       <section id="about" className="container mx-auto px-4 py-16">
         <div className="max-w-4xl mx-auto text-center">
           <h2 className="text-3xl font-bold mb-6">About Me</h2>
+
           <p className="text-xl text-gray-300 mb-4">
-            I am a passionate Full Stack Developer with experience in building
-            web applications using modern technologies. I enjoy solving complex
-            problems and creating user-friendly interfaces.
+            I am a recent graduate of the University of Lynchburg, where I
+            double majored in Computer Science and Data Science, earning a
+            Bachelor of Science degree, along with a minor in Mathematics.
           </p>
-          <p className="text-xl text-gray-300">
-            My journey in the tech world started with a curiosity for how things
-            work, and it has grown into a fulfilling career. I am always eager
-            to learn new skills and take on new challenges.
+          <p className="text-xl text-gray-300 mb-4">
+            During my academic journey, I developed a deep understanding of
+            algorithms, programming languages, and data manipulation techniques.
+            Through various projects and internships, I gained hands-on
+            experience in developing software solutions and extracting valuable
+            insights from complex datasets. My double major in Computer Science
+            and Data Science has equipped me with a unique skill set that
+            combines technical proficiency with analytical thinking.
+          </p>
+          <p className="text-xl text-gray-300 mb-4">
+            I am a software engineer with at CloudFit Software in Lynchburg,
+            Virginia who is passionate about helping others through building
+            software solutions. I have experience in building aesthetic web
+            applications using modern technologies and enjoy solving complex
+            problems. Please feel free to reach out to me with any
+            opportunities, networking inquiries, or to learn more about my
+            experiences. I look forward to connecting and discovering how we can
+            create positive outcomes together in the exciting world of
+            technology and data.
           </p>
         </div>
       </section>
