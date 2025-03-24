6ceddc8831cc685bd468d522d85024939abda7d0
Justin Kachornvanich <justkachorn@gmail.com>
2025-03-23 22:00:31 -0400

    added section to projectgrid

--- src/components/project-grid/ProjectGrid.tsx
+++ src/components/project-grid/ProjectGrid.tsx
@@ -10,23 +10,25 @@ interface IProjectGridProps {
 
 export const ProjectGrid = (props: IProjectGridProps) => {
   return (
-    <div className="sm:py-20" id="projects">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="text-center mb-16">
-          <h1 className="text-5xl font-bold mb-4">{props.headerText}</h1>
-        </div>
-        <div className="flex flex-wrap justify-center">
-          {props.projects.slice(0, 9).map((project, index) => (
-            <ProjectCard
-              key={index}
-              id={project.id.toString()}
-              project={project}
-              headerText={props.headerText}
-              subHeaderText={props.subHeaderText}
-            />
-          ))}
+    <section id={props.id}>
+      <div className="sm:py-20" id="projects">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="text-center mb-16">
+            <h1 className="text-5xl font-bold mb-4">{props.headerText}</h1>
+          </div>
+          <div className="flex flex-wrap justify-center">
+            {props.projects.slice(0, 9).map((project, index) => (
+              <ProjectCard
+                key={index}
+                id={project.id.toString()}
+                project={project}
+                headerText={props.headerText}
+                subHeaderText={props.subHeaderText}
+              />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
